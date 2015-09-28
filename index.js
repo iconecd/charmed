353dfb29396aa31cbc021691859d479add6fe586
Eric Davis <eric.davis@iconstituent.com>
2015-09-28 12:12:28 -0700

    unicode support, config improvements

--- index.js
+++ index.js
@@ -1,41 +1,59 @@
-var fs = require('fs');
-var path = require('path');
+var fs   = require('fs'),
+	path = require('path');
 
-var chars = JSON.parse( fs.readFileSync( path.resolve( __dirname + '/chars.json' ), { encoding: 'utf16le' } ) );
+var charming = function ( cfg ) {
+	var chars = JSON.parse( fs.readFileSync( path.resolve( __dirname + '/chars.json' ), { encoding: 'utf16le' } ) );
 
-var options = {
-	ascii: true,
-	html: true,
-	raw: true,
-	simple: false,
-	unicode: true,
-	disableWarning: false
-};
+	options = {
+		ascii: true,
+		html: true,
+		raw: true,
+		simple: false,
+		unicode: true,
+		dotnet: false,
+		disableWarning: false
+	};
 
-var clean = {
-	ascii: [],
-	html: [],
-	raw: [],
-	simple: [],
-	unicode: []
-};
+	var clean = {
+		ascii: [],
+		html: [],
+		raw: [],
+		simple: [],
+		unicode: [],
+		dotnet: []
+	};
 
-chars.forEach( function ( val, ix, arr ) {
-	clean.ascii.push( String.fromCharCode( val.ascii ) );
-	clean.html.push( '&' + val.html + ';' );
-	clean.raw.push( val.raw );
-	clean.simple.push( val.simple );
-	clean.unicode.push( '\\u' + String( "0000" + val.unicode ).slice( -4 ) );
-} );
+	chars.forEach( function ( val, ix, arr ) {
+		clean.ascii.push( String.fromCharCode( val.ascii ) );
+		clean.html.push( '&' + val.html + ';' );
+		clean.raw.push( val.raw );
+		clean.simple.push( val.simple );
+		clean.unicode.push( '\\u' + String( "0000" + val.unicode ).slice( -4 ) );
+		clean.dotnet.push( '%u' + String( "0000" + val.unicode ).slice( -4 ) );
+	} );
 
-var charming = function () {
-	var self = this;
+	if ( cfg ) {
+		Object.keys( options ).forEach( function ( opt ) {
+			if ( cfg.hasOwnProperty( opt ) ) {
+				options[ opt ] = cfg[ opt ];
+			}
+		});
+	}
 
 	function convert ( input, source, target ) {
+		if ( !input.split ) {
+			return input;
+		}
 		output = input;
+		if ( source == clean.html && target == clean.raw ) {
+			output = output.replace( /&amp;([a-z0-9]+;)/ig, '&$1' );
+		}
 		source.forEach( function ( val, ix, arr ) {
 			output = output.split( val ).join( target[ ix ] );
 		} );
+		if ( source == clean.raw && target == clean.html ) {
+			output = output.replace( /&amp;([a-z0-9]+;)/ig, '&$1' );
+		}
 		return output;
 	}
 
@@ -62,6 +80,10 @@ var charming = function () {
 		return convert( input, clean.ascii, clean.unicode );
 	};
 
+	this.asciiToDotnet = function ( input ) {
+		return convert( input, clean.ascii, clean.dotnet );
+	};
+
 	/* html */
 	this.htmlToAscii = function ( input ) {
 		return convert( input, clean.html, clean.ascii );
@@ -79,6 +101,10 @@ var charming = function () {
 		return convert( input, clean.html, clean.unicode );
 	};
 
+	this.htmlToDotnet = function ( input ) {
+		return convert( input, clean.html, clean.dotnet );
+	};
+
 	/* raw */
 	this.rawToAscii = function ( input ) {
 		return convert( input, clean.raw, clean.ascii );
@@ -94,6 +120,10 @@ var charming = function () {
 
 	this.rawToUnicode = function ( input ) {
 		return convert( input, clean.raw, clean.unicode );
+	};
+
+	this.rawToDotnet = function ( input ) {
+		return convert( input, clean.raw, clean.dotnet );
 	};
 
 	/* simple */
@@ -117,6 +147,11 @@ var charming = function () {
 		return convert( input, clean.simple, clean.unicode );
 	};
 
+	this.simpleToDotnet = function ( input ) {
+		simpleWarning();
+		return convert( input, clean.simple, clean.dotnet );
+	};
+
 	/* unicode */
 	this.unicodeToAscii = function ( input ) {
 		return convert( input, clean.unicode, clean.ascii );
@@ -134,20 +169,48 @@ var charming = function () {
 		return convert( input, clean.unicode, clean.simple );
 	};
 
+	this.unicodeToDotnet = function ( input ) {
+		return convert( input, clean.unicode, clean.dotnet );
+	};
+
+	/* dotnet */
+	this.dotnetToAscii = function ( input ) {
+		return convert( input, clean.dotnet, clean.ascii );
+	};
+
+	this.dotnetToHtml = function ( input ) {
+		return convert( input, clean.dotnet, clean.html );
+	};
+
+	this.dotnetToRaw = function ( input ) {
+		return convert( input, clean.dotnet, clean.raw );
+	};
+
+	this.dotnetToSimple = function ( input ) {
+		return convert( input, clean.dotnet, clean.simple );
+	};
+
+	this.dotnetToUnicode = function ( input ) {
+		return convert( input, clean.dotnet, clean.unicode );
+	};
+
 	/* convenience */
 	this.toAscii = function ( input ) {
 		var output = input;
 		if ( options.html === true ) {
-			output = self.htmlToAscii( output );
+			output = this.htmlToAscii( output );
 		}
 		if ( options.raw === true ) {
-			output = self.rawToAscii( output );
+			output = this.rawToAscii( output );
 		}
 		if ( options.simple === true ) {
-			output = self.simpleToAscii( output );
+			output = this.simpleToAscii( output );
 		}
 		if ( options.unicode === true ) {
-			output = self.unicodeToAscii( output );
+			output = this.unicodeToAscii( output );
+		}
+		if ( options.dotnet === true ) {
+			output = this.dotnetToAscii( output );
 		}
 		return output;
 	};
@@ -155,33 +218,41 @@ var charming = function () {
 	this.toHtml = function ( input ) {
 		var output = input;
 		if ( options.ascii === true ) {
-			output = self.asciiToHtml( output );
+			output = this.asciiToHtml( output );
 		}
 		if ( options.raw === true ) {
-			output = self.rawToHtml( output );
+			output = this.rawToHtml( output );
 		}
 		if ( options.simple === true ) {
-			output = self.simpleToHtml( output );
+			output = this.simpleToHtml( output );
 		}
 		if ( options.unicode === true ) {
-			output = self.unicodeToHtml( output );
+			output = this.unicodeToHtml( output );
 		}
+		if ( options.dotnet === true ) {
+			output = this.dotnetToHtml( output );
+		}
+		/* special case: double-escaped ampersand cleanup */
+		output.replace( /&amp;([a-z0-9]+;)/ig, '&$1', 'ig' );
 		return output;
 	};
 
 	this.toRaw = function ( input ) {
 		var output = input;
 		if ( options.ascii === true ) {
-			output = self.asciiToRaw( output );
+			output = this.asciiToRaw( output );
 		}
 		if ( options.html === true ) {
-			output = self.htmlToRaw( output );
+			output = this.htmlToRaw( output );
 		}
 		if ( options.simple === true ) {
-			output = self.simpleToRaw( output );
+			output = this.simpleToRaw( output );
 		}
 		if ( options.unicode === true ) {
-			output = self.unicodeToRaw( output );
+			output = this.unicodeToRaw( output );
+		}
+		if ( options.dotnet === true ) {
+			output = this.dotnetToRaw( output );
 		}
 		return output;
 	};
@@ -189,16 +260,19 @@ var charming = function () {
 	this.toSimple = function ( input ) {
 		var output = input;
 		if ( options.ascii === true ) {
-			output = self.asciiToSimple( output );
+			output = this.asciiToSimple( output );
 		}
 		if ( options.html === true ) {
-			output = self.htmlToSimple( output );
+			output = this.htmlToSimple( output );
 		}
 		if ( options.raw === true ) {
-			output = self.rawToSimple( output );
+			output = this.rawToSimple( output );
 		}
 		if ( options.unicode === true ) {
-			output = self.unicodeToSimple( output );
+			output = this.unicodeToSimple( output );
+		}
+		if ( options.dotnet === true ) {
+			output = this.dotnetToSimple( output );
 		}
 		return output;
 	};
@@ -206,28 +280,44 @@ var charming = function () {
 	this.toUnicode = function ( input ) {
 		var output = input;
 		if ( options.ascii === true ) {
-			output = self.asciiToUnicode( output );
+			output = this.asciiToUnicode( output );
 		}
 		if ( options.html === true ) {
-			output = self.htmlToUnicode( output );
+			output = this.htmlToUnicode( output );
 		}
 		if ( options.raw === true ) {
-			output = self.rawToUnicode( output );
+			output = this.rawToUnicode( output );
 		}
 		if ( options.simple === true ) {
-			output = self.simpleToUnicode( output );
+			output = this.simpleToUnicode( output );
+		}
+		if ( options.dotnet === true ) {
+			output = this.dotnetToUnicode( output );
 		}
 		return output;
 	};
 
-	this.config = function ( configOpts ) {
-		var opts = Object.keys( options );
-		for ( var opt in opts ) {
-			if ( configOpts.hasOwnProperty( opt ) ) {
-				options[ opt ] = configOpts[ opt ];
-			}
+	this.toDotnet = function ( input ) {
+		var output = input;
+		if ( options.ascii === true ) {
+			output = this.asciiToDotnet( output );
 		}
+		if ( options.html === true ) {
+			output = this.htmlToDotnet( output );
+		}
+		if ( options.raw === true ) {
+			output = this.rawToDotnet( output );
+		}
+		if ( options.simple === true ) {
+			output = this.simpleToDotnet( output );
+		}
+		if ( options.dotnet === true ) {
+			output = this.dotnetToDotnet( output );
+		}
+		return output;
 	};
+
+	return this;
 };
 
-module.exports = new charming();
+module.exports = charming;
